b2326fc521c7d720100d3a863dcea1616a1f96b7
Git Server Admin <admin@git-server>
2021-09-12 11:41:43 +0000

    Add initial repository

---
+++ ts-express-mocha/cypress/integration/get.lists.spec.ts
@@ -0,0 +1,98 @@
+const apiUrl = `${Cypress.env("apiUrl")}`
+import users from '../../../data/users.json';
+
+describe('Get Lists from User Spec', () => {
+
+    const userOwner: any = users[1]
+    const userNotOwner: any = users[2]
+    const basePath: string = `/users/${userOwner['id']}/lists`
+
+    const addSongsPayload: any = {
+        list: {
+            songs: [
+                {
+                    artist: "i'm the artist",
+                    title: "this is my title"
+                }
+            ]
+        }
+    }
+
+    it('should return a list of songs if userOwner is the owner', () => {
+        cy.request({
+            failOnStatusCode: false,
+            method: 'GET',
+            url: `${apiUrl}${basePath}`,
+            auth: {
+                user: userOwner['name'],
+                password: userOwner['password'],
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            expect(response.body).to.be.an('array')
+        })
+    })
+
+    it('should return 401 if not owner of that list', () => {
+        cy.request({
+            failOnStatusCode: false,
+            method: 'GET',
+            url: `${apiUrl}${basePath}`,
+            auth: {
+                user: userNotOwner['name'],
+                password: userNotOwner['password'],
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(401)
+        })
+    })
+
+    it('should return 401 if the user id does not exist', () => {
+        const basePath: string = '/users/fake/lists'
+
+        cy.request({
+            failOnStatusCode: false,
+            method: 'GET',
+            url: `${apiUrl}${basePath}`,
+            auth: {
+                user: userNotOwner['name'],
+                password: userNotOwner['password'],
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(401)
+        })
+    })
+
+    it('should contain the inserted song on the list', () => {
+        let listId: string
+
+        cy.request({
+            failOnStatusCode: false,
+            method: 'POST',
+            url: `${apiUrl}${basePath}`,
+            body: addSongsPayload,
+            auth: {
+                user: userOwner['name'],
+                password: userOwner['password'],
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            addSongsPayload.list.listId = response.body.listId
+        })
+
+        cy.request({
+            failOnStatusCode: false,
+            method: 'GET',
+            url: `${apiUrl}${basePath}`,
+            auth: {
+                user: userOwner['name'],
+                password: userOwner['password'],
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            expect(response.body.map(x => x.listId)).to.contain(addSongsPayload.list.listId)
+            expect(response.body.map(x => x.songs)).to.deep.contain(addSongsPayload.list.songs)
+        })
+    })
+
+})
---
+++ ts-express-mocha/src/test/main.test.ts
@@ -0,0 +1,7 @@
+import { expect } from 'chai';
+
+describe('Main test', () => { // the tests container
+    it('should return 1', () => { // the single test
+        expect(1).to.equal(1);
+    });
+});
