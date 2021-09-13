16442cf6d28f48476be2f197c2ce04be8afe71cb
devtoni <devtonirm@gmail.com>
2021-09-13 22:19:54 +0200

    :white_check_mark: API testing with Cypress

--- spotlist-api/cypress/integration/add.list.spec.ts
+++ spotlist-api/cypress/integration/add.list.spec.ts
@@ -1,86 +1,85 @@
-const apiUrl = `${Cypress.env("apiUrl")}`
-import users from '../../../data/users.json';
+const apiUrl = `${Cypress.env('apiUrl')}`;
+import users from '../fixtures/users.json';
 
 describe('Create List Spec', () => {
+  const user: any = users[1];
+  const basePath: string = `/users/${user['id']}/lists`;
 
-    const user: any = users[1]
-    const basePath: string = `/users/${user['id']}/lists`
-
-    const addListsPayload: any = {
-        list: {
-            songs: [
-                {
-                    artist: "artist1",
-                    title : "title1"
-                },
-                {
-                    artist: "artist2",
-                    title: "title2"
-                }
-            ]
+  const addListsPayload: any = {
+    list: {
+      songs: [
+        {
+          artist: 'artist1',
+          title: 'title1'
+        },
+        {
+          artist: 'artist2',
+          title: 'title2'
         }
+      ]
     }
+  };
 
-    it('should create a list with valid object', () => {
-        cy.request({
-            failOnStatusCode: false,
-            method: 'POST',
-            url: `${apiUrl}${basePath}`,
-            body: addListsPayload,
-            auth: {
-                user: user['name'],
-                password: user['password'],
-            }
-        }).then((response) => {
-            expect(response.status).to.eq(200)
-            expect(response.body).to.have.property('listId')
-            expect(response.body.songs).to.deep.equal(addListsPayload.list.songs)
-        })
-    })
+  it('should create a list with valid object', () => {
+    cy.request({
+      failOnStatusCode: false,
+      method: 'POST',
+      url: `${apiUrl}${basePath}`,
+      body: addListsPayload,
+      auth: {
+        user: user['name'],
+        password: user['password']
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property('listId');
+      expect(response.body.songs).to.deep.equal(addListsPayload.list.songs);
+    });
+  });
 
-    it('should give a 400 with invalid params', () => {
-        cy.request({
-            failOnStatusCode: false,
-            method: 'POST',
-            url: `${apiUrl}${basePath}`,
-            body: {},
-            auth: {
-                user: user['name'],
-                password: user['password'],
-            }
-        }).then((response) => {
-            expect(response.status).to.eq(400)
-        })
-    })
+  it('should give a 400 with invalid params', () => {
+    cy.request({
+      failOnStatusCode: false,
+      method: 'POST',
+      url: `${apiUrl}${basePath}`,
+      body: {},
+      auth: {
+        user: user['name'],
+        password: user['password']
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+    });
+  });
 
-    it('should give a 401 with non-authorized user', () => {
-        const fakeBasePath: string = '/users/1111/lists'
-        cy.request({
-            failOnStatusCode: false,
-            method: 'POST',
-            url: `${apiUrl}${fakeBasePath}`,
-            body: addListsPayload,
-            auth: {
-                user: user['name'],
-                password: user['password'],
-            }
-        }).then((response) => {
-            expect(response.status).to.eq(401)
-        })
-    })
+  it('should give a 401 with non-authorized user', () => {
+    const fakeBasePath: string = '/users/1111/lists';
+    cy.request({
+      failOnStatusCode: false,
+      method: 'POST',
+      url: `${apiUrl}${fakeBasePath}`,
+      body: addListsPayload,
+      auth: {
+        user: user['name'],
+        password: user['password']
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(401);
+    });
+  });
 
-    it('should give a 401 with non-existing user', () => {
-        cy.request({
-            failOnStatusCode: false,
-            method: 'POST',
-            url: `${apiUrl}${basePath}`,
-            body: addListsPayload,
-            auth: {
-                user: 'aaaa',
-                password: 'bbbb',
-            }
-        }).then((response) => {
-            expect(response.status).to.eq(401)
-        })
-    })
-})
+  it('should give a 401 with non-existing user', () => {
+    cy.request({
+      failOnStatusCode: false,
+      method: 'POST',
+      url: `${apiUrl}${basePath}`,
+      body: addListsPayload,
+      auth: {
+        user: 'aaaa',
+        password: 'bbbb'
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(401);
+    });
+  });
+});
--- spotlist-api/cypress/integration/add.songs.spec.ts
+++ spotlist-api/cypress/integration/add.songs.spec.ts
@@ -1,116 +1,115 @@
-const apiUrl = `${Cypress.env("apiUrl")}`
-import users from '../../../data/users.json';
+const apiUrl = `${Cypress.env('apiUrl')}`;
+import users from '../fixtures/users.json';
 
 describe('Add Songs to List Spec', () => {
+  const user: any = users[1];
+  const addListBasePath: string = `/users/${user['id']}/lists`;
+  let listId: string;
+  let addSongBasePath: string;
 
-    const user: any = users[1]
-    const addListBasePath: string = `/users/${user['id']}/lists`
-    let listId: string
-    let addSongBasePath: string
-
-    const addListsPayload: any = {
-        list: {
-            songs: [
-                {
-                    artist: "artist1",
-                    title : "title1"
-                },
-                {
-                    artist: "artist2",
-                    title: "title2"
-                }
-            ]
+  const addListsPayload: any = {
+    list: {
+      songs: [
+        {
+          artist: 'artist1',
+          title: 'title1'
+        },
+        {
+          artist: 'artist2',
+          title: 'title2'
         }
+      ]
     }
+  };
 
-    const addSongPayload: any = {
-        song: {
-            artist: "artistname",
-            title: "songtitle"
-        }
+  const addSongPayload: any = {
+    song: {
+      artist: 'artistname',
+      title: 'songtitle'
     }
+  };
 
-    before(() => {
-        cy.request({
-            failOnStatusCode: false,
-            method: 'POST',
-            url: `${apiUrl}${addListBasePath}`,
-            body: addListsPayload,
-            auth: {
-                user: user['name'],
-                password: user['password'],
-            }
-        }).then((response) => {
-            expect(response.status).to.eq(200)
-            listId = response.body.listId
-            addSongBasePath = `/users/${user['id']}/lists/${listId}/songs`
-        })
-    })
+  before(() => {
+    cy.request({
+      failOnStatusCode: false,
+      method: 'POST',
+      url: `${apiUrl}${addListBasePath}`,
+      body: addListsPayload,
+      auth: {
+        user: user['name'],
+        password: user['password']
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      listId = response.body.listId;
+      addSongBasePath = `/users/${user['id']}/lists/${listId}/songs`;
+    });
+  });
 
-    it('should add a song to an existing list', () => {
-        cy.request({
-            failOnStatusCode: false,
-            method: 'POST',
-            url: `${apiUrl}${addSongBasePath}`,
-            body: addSongPayload,
-            auth: {
-                user: user['name'],
-                password: user['password'],
-            }
-        }).then((response) => {
-            expect(response.status).to.eq(200)
-            expect(response.body).to.deep.equal(addSongPayload.song)
-        })
-    })
+  it('should add a song to an existing list', () => {
+    cy.request({
+      failOnStatusCode: false,
+      method: 'POST',
+      url: `${apiUrl}${addSongBasePath}`,
+      body: addSongPayload,
+      auth: {
+        user: user['name'],
+        password: user['password']
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.deep.equal(addSongPayload.song);
+    });
+  });
 
-    it('should throw 400 if list id does not exist', () => {
-        const randomListIdBasePath: string = `/users/${user['id']}/lists/1111111/songs`
+  it('should throw 400 if list id does not exist', () => {
+    const randomListIdBasePath: string = `/users/${user['id']}/lists/1111111/songs`;
 
-        cy.request({
-            failOnStatusCode: false,
-            method: 'POST',
-            url: `${apiUrl}${randomListIdBasePath}`,
-            body: addSongPayload,
-            auth: {
-                user: user['name'],
-                password: user['password'],
-            }
-        }).then((response) => {
-            expect(response.status).to.eq(400)
-        })
-    })
+    cy.request({
+      failOnStatusCode: false,
+      method: 'POST',
+      url: `${apiUrl}${randomListIdBasePath}`,
+      body: addSongPayload,
+      auth: {
+        user: user['name'],
+        password: user['password']
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+    });
+  });
 
-    it('should throw 401 if user does not have permissions to the list', () => {
-        const nonOwnerUser: any = users[2]
+  it('should throw 401 if user does not have permissions to the list', () => {
+    const nonOwnerUser: any = users[2];
 
-        cy.request({
-            failOnStatusCode: false,
-            method: 'POST',
-            url: `${apiUrl}${addSongBasePath}`,
-            body: addSongPayload,
-            auth: {
-                user: nonOwnerUser['name'],
-                password: nonOwnerUser['password'],
-            }
-        }).then((response) => {
-            expect(response.status).to.eq(401)
-        })
-    })
+    cy.request({
+      failOnStatusCode: false,
+      method: 'POST',
+      url: `${apiUrl}${addSongBasePath}`,
+      body: addSongPayload,
+      auth: {
+        user: nonOwnerUser['name'],
+        password: nonOwnerUser['password']
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(401);
+    });
+  });
 
-    it('should throw 401 if user does not exist', () => {
-        const user: any = users[0]
+  it('should throw 401 if user does not exist', () => {
+    const user: any = users[0];
 
-        cy.request({
-            failOnStatusCode: false,
-            method: 'POST',
-            url: `${apiUrl}${addSongBasePath}`,
-            body: addSongPayload,
-            auth: {
-                user: user['name'],
-                password: user['password'],
-            }
-        }).then((response) => {
-            expect(response.status).to.eq(401)
-        })
-    })
-})
+    cy.request({
+      failOnStatusCode: false,
+      method: 'POST',
+      url: `${apiUrl}${addSongBasePath}`,
+      body: addSongPayload,
+      auth: {
+        user: user['name'],
+        password: user['password']
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(401);
+    });
+  });
+});
